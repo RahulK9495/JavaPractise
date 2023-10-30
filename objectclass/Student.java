1f5dc7f0af40ada458c17ce5ec98ada7c4de3d2e
RahulK9495 <rahulkhot5554@gmail.com>
2023-10-30 16:09:33 +0530

    new commit

--- objectclass/Student.java
+++ objectclass/Student.java
@@ -5,7 +5,7 @@ public class Student implements Cloneable {
 	int roll;
 	String name;
 	Add address;
-	
+
 	public Student() {
 		super();
 		// TODO Auto-generated constructor stub
@@ -17,34 +17,33 @@ public class Student implements Cloneable {
 		this.name = name;
 		this.address = address;
 	}
-	
+
 	public static void main(String[] args) throws CloneNotSupportedException {
-		
-		Add ad=new Add(416215,"Pune");
-		ad.City="Mumbai";
-		
-		Student s1=new Student(3,"Ram",ad);
-		
-		Student s2=(Student) s1.clone();
-		
-		System.out.println(s1==s2);
-		
-		System.out.println(s1.address==s2.address);
-		System.out.println(s1.roll==s2.roll);
-		
+
+		Add ad = new Add(416215, "Pune");
+		ad.City = "Mumbai";
+
+		Student s1 = new Student(3, "Ram", ad);
+
+		Student s2 = (Student) s1.clone();
+
+		System.out.println(s1 == s2);
+
+		System.out.println(s1.address == s2.address);
+		System.out.println(s1.roll == s2.roll);
+
 		System.out.println("=========S1==========");
-		s2.address.City="dubai";
+		s2.address.City = "dubai";
 		System.out.println(s1.roll);
 		System.out.println(s1.name);
 		System.out.println(s1.address.pin);
 		System.out.println(s1.address.City);
-	
-		System.out.println("=========S1==========");
+
+		System.out.println("=========s2==========");
 		System.out.println(s2.roll);
 		System.out.println(s2.name);
 		System.out.println(s2.address.pin);
 		System.out.println(s2.address.City);
 	}
-	
-	
+
 }
