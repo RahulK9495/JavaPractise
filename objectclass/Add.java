b2f2373bedc3ae0a82ca29ff6fab878678b611fc
RahulK9495 <rahulkhot5554@gmail.com>
2023-10-30 16:10:23 +0530

    format change

--- objectclass/Add.java
+++ objectclass/Add.java
@@ -4,16 +4,16 @@ public class Add {
 
 	int pin;
 	String City;
+
 	public Add() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public Add(int pin, String address) {
 		super();
 		this.pin = pin;
 		City = address;
 	}
-	
-	
+
 }
