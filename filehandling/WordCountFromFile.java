06a8d4213edf7d6f4eb5cda3ec9d25aa1d0e0efe
RahulK9495 <rahulkhot5554@gmail.com>
2024-01-22 15:15:11 +0530

    new commit

--- filehandling/WordCountFromFile.java
+++ filehandling/WordCountFromFile.java
@@ -10,38 +10,33 @@ public class WordCountFromFile {
 
 	public static void main(String[] args) {
 
-		int charcount =0;
-		int wordcount =0;
-		int linecount =0;
-		BufferedReader br=null;
-		try 
-		{
-			br=new BufferedReader(new FileReader(new File("abc.txt")));
-			
-			String currentline= br.readLine();
-			
-			while(currentline!=null)
-			{
+		int charcount = 0;
+		int wordcount = 0;
+		int linecount = 0;
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(new File("abc.txt")));
+
+			String currentline = br.readLine();
+
+			while (currentline != null) {
 				linecount++;
-				
-				String[]words = currentline.split(" ");
-				
-				wordcount+= words.length;
-				
-				for (String word:words)
-				{
-					charcount = charcount+words.length;
+
+				String[] words = currentline.split(" ");
+
+				wordcount += words.length;
+
+				for (String word : words) {
+					charcount = charcount + words.length;
 				}
-				
-				currentline=br.readLine();
+
+				currentline = br.readLine();
 			}
-		
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		finally
-		{
+		} finally {
 			try {
 				br.close();
 			} catch (IOException e) {
@@ -50,10 +45,10 @@ public class WordCountFromFile {
 			}
 			System.out.println();
 		}
-		
-		System.out.println("The total words present in the file "+wordcount);
-		System.out.println("The lines words present in the file "+linecount);
-		System.out.println("The characters words present in the file "+charcount);
+
+		System.out.println("The total words present in the file " + wordcount);
+		System.out.println("The lines words present in the file " + linecount);
+		System.out.println("The characters words present in the file " + charcount);
 	}
 
 }
